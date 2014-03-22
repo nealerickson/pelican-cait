1cb07b25ae88d46d8aabb2b113b77e7019a22f53
Neal Erickson <neal@heedworks.com>
2014-03-22 03:05:10 -0400

    Looks of template and style updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,31 @@
 // Empty for now. Maybe used later.
+var window$ = $(window)
+
+$(function() {
+	var isDescShowing = false;
+	
+
+	$('#toggle-site-desc').on('click', function() {
+		var el$ = $(this);
+		var desc$ = $('#site-desc-wrapper');
+
+		event.preventDefault();
+
+		console.log(event);
+		
+		if (desc$.hasClass('show-desc')) {
+			desc$.removeClass('show-desc');
+		} else {
+			desc$.addClass('show-desc');
+		}		
+	});
+
+	/*window$.resize(function() {
+		//if (isDescShowing) return;
+		var width = window$.width();
+		if (width > 800) {
+			desc$.show();
+			//isDescShowing = true;
+		}	    
+	});*/
+});
